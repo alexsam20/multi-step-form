21cb06e0d10a22a0e2c6b01a906118052a059a7d
Alex SaM <alsam3637@gmail.com>
2026-02-27 19:04:05 -0500

    Step progress

--- script.js
+++ script.js
@@ -1,12 +1,43 @@
 document.addEventListener('DOMContentLoaded', () => {
-    /* Сначала я получу корневой элемент формы с классом .form-wizard */
+    /* 1. Сначала я получу корневой элемент формы из класса .form-wizard */
     const form = document.querySelector('.form-wizard');
-    /* Затем я выделяю все элементы <li> в селекторе .progress-container */
+    /* 9. Я получуу элемент .progress */
+    const progress = form.querySelector('.progress')
+    /* 2. Затем я выделяю все элементы <li> в классе .progress-container */
     const stepIndicators = form.querySelectorAll('.progress-container li');
     console.log(form);
     console.log(stepIndicators);
     console.log(stepIndicators.length);
-    /* Затем я получу корневой элемент и переопределю переменную --steps в CSS */
+    /* 3. Затем я получу корневой элемент и переопределю переменную --steps в CSS */
     document.documentElement.style.setProperty("--steps", stepIndicators.length);
+    /* 4. Здесь я определяю текущий step и переношу его в индекс первого шага */
+    let currentStep = 0;
+    /* 5. Далее, я создам функцию, которая будет имитировать последовательность шагов */
+    setInterval(() => {
+
+        currentStep++;
+
+        console.log(currentStep);
+
+        if(currentStep > stepIndicators.length - 1) {
+            currentStep = 0;
+        }
+
+        /* 10. Внутри функции я я рассчитываю процент ширины и зависимости от текущего шага (currentStep)  */
+        let width = currentStep / (stepIndicators.length - 1);
+        console.log(width);
+        /* Я использую полученный результат для изменения длины элемента индикатора выполнения */
+        progress.style.transform = `scaleX(${width})`;
+
+
+        /* 6. Внутри функции я буду проходить циклом каждый этап выполнения */
+        stepIndicators.forEach((indicator, index) => {
+            /* 7. Затем я изменю текущий ('current') класс для каждого из них */
+            indicator.classList.toggle('current', currentStep === index);
+            /* 8. Изменяю состояние класса на "Готово" ('done') */
+            indicator.classList.toggle('done', currentStep > index);
+        });
+
+    }, 2000);
 
 });
